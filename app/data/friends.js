2fb4da4055e47b64ef201c13634e48a394485439
Chris Warner <christopher.warner92@gmail.com>
2019-04-13 08:44:03 -0400

    Added modal window title.
    Added modal window title.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,8 +4,20 @@ var friends = [
     {
         //Test
         name:"Test",
-        photo:"",
-        scores:[1,2,3,4]
+        photo:"https://unsplash.com/photos/ilUWHlGndAA",
+        scores:
+        [
+            1,
+            2,
+            3,
+            4,
+            1,
+            2,
+            3,
+            4,
+            4,
+            3
+        ]
     }
 ];
 
