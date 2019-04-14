eb0de7b2d86469602c349deaaa0eb07b1a4b214d
Chris Warner <christopher.warner92@gmail.com>
2019-04-14 04:09:01 -0400

    added code to update modal window with best match details. App is now complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,8 @@
-var friends = [
-    {
+var friends = [{
         //Test
-        name:"Test",
-        photo:"https://unsplash.com/photos/ilUWHlGndAA",
-        scores:
-        [
+        name: "Anne",
+        photo: "https://images.unsplash.com/photo-1524253860834-0e02422248bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80",
+        scores: [
             1,
             2,
             3,
@@ -16,6 +14,22 @@ var friends = [
             4,
             3
         ]
+    },
+    {
+        name: "Julie",
+        photo: "https://images.unsplash.com/photo-1498652774327-af8352e87862?ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80",
+        scores: [
+            4,
+            1,
+            5,
+            2,
+            4,
+            5,
+            1,
+            1,
+            1,
+            5
+        ],
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,31 +11,31 @@ var newPerson;
 
 var friendsDataArray = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
+var bestMatch;
 
-    
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData);
     console.log("friends data" + friendsData)
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     let matchArray = [];
     //containts the friends array==================
     console.log("NEWSUBMISSION==================")
-     newPerson = req.body; // data from survey
-     console.log("Survey data submited: ");
-     console.log(newPerson);    
-     console.log("People in database: ");
-     console.log( friendsDataArray);
-     console.log("Calculating difference between each score between survey submission and people in database"); 
-  // The scores is in this index will be in allignment with the persons in friendsDataArray.
-     friendsDataArray.forEach(function (person){
+    newPerson = req.body; // data from survey
+    console.log("Survey data submited: ");
+    console.log(newPerson);
+    console.log("People in database: ");
+    console.log(friendsDataArray);
+    console.log("Calculating difference between each score between survey submission and people in database");
+    // The scores is in this index will be in allignment with the persons in friendsDataArray.
+    friendsDataArray.forEach(function (person) {
 
-       let talley = 0;
-       console.log(person.name);
-       console.log(newPerson.name); 
-      for(var i = 0; i < 10; i ++){ 
+      let talley = 0;
+      console.log(person.name);
+      console.log(newPerson.name);
+      for (var i = 0; i < 10; i++) {
         let scoreResult = Math.abs(person.scores[i] - newPerson.scores[i]);
         console.log(scoreResult);
         talley = talley + scoreResult;
@@ -45,16 +45,18 @@ module.exports = function(app) {
       matchArray.push(talley);
       console.log("Results Talley Array Databse");
       console.log(matchArray);
-     });
+    });
 
-     let i = matchArray.indexOf(Math.max(...matchArray));
-     console.log("Position of best match in best match array...");
-     console.log(i);
-     console.log("RESULTS");
-     console.log("YOUR BEST MATCH IS");
-     console.log(friendsDataArray[i].name);
+    let i = matchArray.indexOf(Math.max(...matchArray));
+    console.log("Position of best match in best match array...");
+    console.log(i);
+    console.log("RESULTS");
+    console.log("YOUR BEST MATCH IS");
+    console.log(friendsDataArray[i].name);
 
-     friendsDataArray.push(newPerson);//add person to friends array.
-
-});
+    
+    friendsDataArray.push(newPerson); //add person to friends array.
+res.send(friendsDataArray[i]);
+  });
+  //friendsDataArray[i];
 }
