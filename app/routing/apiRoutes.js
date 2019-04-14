8b1c9e986b9263406c8f7ee70cd465a8ae0ae706
Chris Warner <christopher.warner92@gmail.com>
2019-04-13 21:58:44 -0400

    working on algorithm for calculating best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,23 +3,34 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require("path");
-
+var newPerson;
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-var friendsData = require("../data/friends");
+var friendsDataArray = require("../data/friends");
 
 module.exports = function(app) {
 
     
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
+    console.log("friends data" + friendsData)
   });
 
 
   app.post("/api/friends", function(req, res) {
-    res.json(friendsData);
+    //containts the friends array==================
+    console.log("NEWSUBMISSION==================")
+     newPerson = req.body; // data from survey
+     console.log("Survey data submited: ");
+     console.log(newPerson);    
+     friendsDataArray.push(newPerson);//add person to friends array.
+
+     console.log("People in database: ");
+     console.log( friendsDataArray);
+
+
 });
 }
